273382f0511cd24e5e755a239211e86a405e3a07
zihaowangusa <50635536+zihaowangusa@users.noreply.github.com>
2019-06-14 11:10:55 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ class App extends Component {
   
   componentDidMount(){
     //axios.get('').then(res=>res.data).then(data=>{});
+    
     let data={'mylist' : [{'title': 'Futurama','id': 1,'img': 'https://cdn1.nflximg.net/webp/7621/3787621.webp'},{'title': 'The Interview','id': 2,'img': 'https://cdn1.nflximg.net/webp/1381/11971381.webp'},
     {'title': 'Gilmore Girls','id': 3,'img': 'https://cdn1.nflximg.net/webp/7451/11317451.webp'}],
     'recommendations' : [{'title': 'Family Guy','id': 4,'img': 'https://cdn5.nflximg.net/webp/5815/2515815.webp'},{'title': 'The Croods','id': 5,'img': 'https://cdn3.nflximg.net/webp/2353/3862353.webp'},
